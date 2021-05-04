bf9bf4402e13e2a16110bcd54a26e8c5f82e00c1
Eñaut Mendiluze <mendienaut@gmail.com>
2021-05-04 15:57:12 +0200

    Change quantum gates number
    Add toffoli gate as ccx

--- QuantumMutation/QuantumGates.py
+++ QuantumMutation/QuantumGates.py
@@ -1,7 +1,7 @@
 
-
-AllGates = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx", "swap", "rzz", "rxx", "cx", "cz") #All gates that are implemented
+AllGates = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx", "swap", "rzz", "rxx", "cx", "cz", "ccx") #All gates that are implemented
 OneQubit = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx") #Gates that only affect one qubit
-ManyQubit = ("swap", "rzz", "rxx", "cx", "cz") #Gates that affect many qubit
+ManyQubit = ("swap", "rzz", "rxx", "cx", "cz", "ccx") #Gates that affect many qubit
+MoreThanTwoQubit = ("ccx",) #Gates that affect more than two qubit
 PhaseGates = ("p", "rx", "ry", "rz", "rzz", "rxx") #Gates that affect the phase and needs to specify a phase
 
