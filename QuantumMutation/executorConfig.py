bf52c593acb802a0201942aa866a6f10b3168028
Eñaut Mendiluze <mendienaut@gmail.com>
2021-05-27 11:35:28 +0200

    Separate Config file

---
+++ QuantumMutation/executorConfig.py
@@ -0,0 +1,4 @@
+#Mutant execution variables
+numShots = 100                #Number of shots in each execution for each input
+allInputs = True             #If this true the program will ignore all the other criteria
+
