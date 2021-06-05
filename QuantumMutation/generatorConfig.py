cf6297b1d772190af1ff9e805c31788b1a97358b
ShaukatAli2013 <shaukat@simula.no>
2021-06-05 15:39:46 +0200

    Update generatorConfig.py
    added description

--- QuantumMutation/generatorConfig.py
+++ QuantumMutation/generatorConfig.py
@@ -1,11 +1,10 @@
+#Mutants Generator Configuration Variables
 import math
-#Mutant creation variables
-maxNumberOfMutants = 100 #Max number of mutants
-operators = ("Add","Replace") #Type of operators are going to use to create mutants
-types = ("OneQubit",) #Types of gates the mutation will change
-gateNum = (1,3) #IDNumber of the gates that mutation is going to change
-gapNum = (1,5) #IDNumber of the gaps that mutation is going to change
-phases = [0,90,180,270] #Phase degree that is going to select randomly
-allMutants = False #If this true the program will ignore all the other criteria
-
+maxNumberOfMutants = 100 # Maximum number of mutants to be generated
+operators = ("Add","Replace") # Type of operators to be use to create mutants. Valid values are Add, Replace, Remove
+types = ("OneQubit",) # Types of gates the mutation will change, Valid values are OneQubit and xx
+gateNum = (1,3) # Gate number to be replaced and removed.
+gapNum = (1,5) # Location number of the gaps that will be used to add gates.
+phases = [0,90,180,270] # Phase degrees that will be used to select one value randomly.
+allMutants = False # If this variable is true, then all the possible mutants will be generated.
 
