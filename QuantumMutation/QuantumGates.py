42a3d7b65511964ec621fc6954a8098999db02c0
ShaukatAli2013 <shaukat@simula.no>
2021-06-05 15:26:04 +0200

    Update QuantumGates.py
    Added more description.

--- QuantumMutation/QuantumGates.py
+++ QuantumMutation/QuantumGates.py
@@ -1,4 +1,8 @@
-
+# This file has two purposes 
+# Mutation Selection: A user can specify the gates to be used for generation of mutants
+# Extension: A user can specify the new gates here and the tool will also use them for mutation generation
+# The default file contains all the current supported gates
+# Note: One must specify a gate in multiple sets. For example, a one qubit gate must be specified both in AllGates and also in OneQubit
 AllGates = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx", "swap", "rzz", "rxx", "cx", "cz", "ccx", "cswap") #All gates that are implemented
 OneQubit = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx") #Gates that only affect one qubit
 ManyQubit = ("swap", "rzz", "rxx", "cx", "cz") #Gates that affect two qubit
