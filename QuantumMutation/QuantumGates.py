a615886766af5664fb05b73f87c2d28f733ae4be
Eñaut Mendiluze Usandizaga <76204514+EnautMendi@users.noreply.github.com>
2021-06-07 16:04:16 +0200

    Delete QuantumMutation directory

--- QuantumMutation/QuantumGates.py
+++
@@ -1,7 +0,0 @@
-
-AllGates = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx", "swap", "rzz", "rxx", "cx", "cz", "ccx", "cswap") #All gates that are implemented
-OneQubit = ("x", "h", "p", "t", "s", "z", "y", "id", "rx", "ry", "rz", "sx") #Gates that only affect one qubit
-ManyQubit = ("swap", "rzz", "rxx", "cx", "cz") #Gates that affect two qubit
-MoreThanTwoQubit = ("ccx", "cswap") #Gates that affect more than two qubit
-PhaseGates = ("p", "rx", "ry", "rz", "rzz", "rxx") #Gates that affect the phase and needs to specify a phase
-
